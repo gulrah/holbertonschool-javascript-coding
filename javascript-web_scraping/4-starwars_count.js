2f3b8b37f777e7f8fcc8f67cfe8071c83e6b68e7
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-19 13:01:54 +0400

    Update 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = process.argv[2];
+const apiUrl = process.argv[2];
 
-request(url, function (error, response, body) {
-    if (error) {
-	console.error(error);
-	return;
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+  const characterId = '18'; // Character ID for "Wedge Antilles"
+  let count = 0;
+
+  films.forEach(film => {
+    if (film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
+      count++;
     }
-    const films = JSON.parse(body).results;
-    const count = films.reduce((acc, film) => {
-	if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-	    return acc + 1;
-	}
-	return acc;
-    }, 0);
-    console.log(count);
+  });
+
+  console.log(count);
 });
