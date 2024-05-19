40ea46ffee66b4f951cf458a3a782a29077d6289
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-19 12:58:14 +0400

    Update 2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,9 +2,9 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response) {
-    if (error) {
-	console.error(error);
-	return;
-    }
-    console.log('code:', response.statusCode);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('code:', response.statusCode);
 });
