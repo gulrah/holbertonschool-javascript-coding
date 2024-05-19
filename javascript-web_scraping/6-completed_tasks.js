39df6d9f4bf54f6f5be699a4ee5762d64efb0e1a
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-19 13:01:10 +0400

    Update 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,20 +4,22 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-    if (error) {
-	console.error(error);
-	return;
+  if (error) {
+    console.error(error);
+    return;
+  }
+  
+  const todos = JSON.parse(body);
+  const completedTasks = todos.reduce((acc, todo) => {
+    if (todo.completed) {
+      if (!acc[todo.userId]) {
+        acc[todo.userId] = 1;
+      } else {
+        acc[todo.userId]++;
+      }
     }
-    const todos = JSON.parse(body);
-    const completedTasks = todos.reduce((acc, todo) => {
-	if (todo.completed) {
-	    if (!acc[todo.userId]) {
-		acc[todo.userId] = 1;
-	    } else {
-		acc[todo.userId]++;
-	    }
-	}
-	return acc;
-    }, {});
-    console.log(completedTasks);
+    return acc;
+  }, {});
+
+  console.log(completedTasks);
 });
