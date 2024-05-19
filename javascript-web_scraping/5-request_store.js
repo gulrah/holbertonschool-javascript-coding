e79e333924fadc252b5dc7b34dcd62ac705663d2
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-19 13:00:33 +0400

    Update 5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,13 +6,13 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, function (error, response, body) {
-    if (error) {
-	console.error(error);
-	return;
+  if (error) {
+    console.error(error);
+    return;
+  }
+  fs.writeFile(filePath, body, 'utf-8', function (err) {
+    if (err) {
+      console.log(err);
     }
-    fs.writeFile(filePath, body, 'utf-8', function (err) {
-	if (err) {
-	    console.log(err);
-	}
-    });
+  });
 });
