32056f82fecedd2a3df8784a48f1ba73fc4de62d
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-19 12:56:22 +0400

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-    if (err) {
-	console.log(err);
-	return;
-    }
-    console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
