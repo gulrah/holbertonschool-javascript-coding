250cd8a2031809ee7182b100ba0c8f85319d8cb1
gulrah <gulnarrahimli@mail.ru>
2024-05-20 13:17:59 +0000

    First Commit

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter(line => line);
+    const students = lines.slice(1).map(line => line.split(','));
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+    students.forEach(student => {
+      const field = student[3];
+      if (!fields[field]) {
+ 	fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    });
+
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
