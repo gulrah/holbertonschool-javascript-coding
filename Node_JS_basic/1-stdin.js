b17d07b151ecb60e9ed15f37d78a923d9a69857c
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-26 23:50:54 +0400

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,6 +2,5 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  process.stdout.write('This important software is now closing\n');
   process.stdin.end();
 });
